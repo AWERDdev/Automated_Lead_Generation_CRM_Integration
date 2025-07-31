72ef92375c7de8637e7791b1efd4cb35a3d5f172
AWERDdev <kokorami565@gmail.com>
2025-07-31 12:09:36 +0300

    lead generator updated with the a custom URL

--- lead_generator/Routes/DOMLeadsRoute.py
+++ lead_generator/Routes/DOMLeadsRoute.py
@@ -7,53 +7,59 @@ router = APIRouter(
     responses={404: {"description": "Not found"}}
 )
 
-router.get('/')
+@router.get('/')
 def Welcome_Message():
     return{"message":"welcome to the DOM Leads Extractor"}
 
 
-@router.get("/Divs", url = str )
-def DOMLEADS():
-    results = DOMLeadExtractor.scrape_leads(url = "https://www.yellowpages.com/search?search_terms=coffee&geo_location",extractors = 'divs' , configs = None )
+@router.get("/Divs/{url}")
+async def DOMLEADS( url: str ):
+    results = DOMLeadExtractor.scrape_leads(url , extractors = 'divs' , configs = None )
     return {"message": "DOM leads scraped","Results":results}
 
 
 @router.get("/paragraphs")
 def DOMLEADS():
-    results = DOMLeadExtractor.scrape_leads(url = "https://www.yellowpages.com/search?search_terms=coffee&geo_location",extractors = 'divs' , configs = None )
+    results = DOMLeadExtractor.scrape_leads(url = "https://www.yellowpages.com/search?search_terms=coffee&geo_location",extractors = 'paragraphs' , configs = None )
    
     return {"message": "DOM leads scraped","Results":results}
 
 
 @router.get("/list_items")
 def DOMLEADS():
-    results = DOMLeadExtractor.scrape_leads(url = "https://www.yellowpages.com/search?search_terms=coffee&geo_location",extractors = 'divs' , configs = None )
+    results = DOMLeadExtractor.scrape_leads(url = "https://www.yellowpages.com/search?search_terms=coffee&geo_location",extractors = 'list_items' , configs = None )
 
     return {"message": "DOM leads scraped","Results":results}
 
 
 @router.get("/images")
 def DOMLEADS():
-    results = DOMLeadExtractor.scrape_leads(url = "https://www.yellowpages.com/search?search_terms=coffee&geo_location",extractors = 'divs' , configs = None )
+    results = DOMLeadExtractor.scrape_leads(url = "https://www.yellowpages.com/search?search_terms=coffee&geo_location",extractors = 'images' , configs = None )
     
     return {"message": "DOM leads scraped","Results":results}
 
 
 @router.get("/extract_from_data_attrs")
 def DOMLEADS():
-    results = DOMLeadExtractor.scrape_leads(url = "https://www.yellowpages.com/search?search_terms=coffee&geo_location",extractors = 'divs' , configs = None )
+    results = DOMLeadExtractor.scrape_leads(url = "https://www.yellowpages.com/search?search_terms=coffee&geo_location",extractors = 'data_attrs' , configs = None ,required_attrs = '' )
     
     return {"message": "DOM leads scraped","Results":results}
 
 @router.get("/extract_from_json_ld")
 def DOMLEADS():
-    results = DOMLeadExtractor.scrape_leads(url = "https://www.yellowpages.com/search?search_terms=coffee&geo_location",extractors = 'divs' , configs = None )
+    results = DOMLeadExtractor.scrape_leads(url = "https://www.yellowpages.com/search?search_terms=coffee&geo_location",extractors = 'json_ld' , configs = None )
     
     return {"message": "DOM leads scraped","Results":results}
 
 @router.get("/extract_from_address")
 def DOMLEADS():
-    results = DOMLeadExtractor.scrape_leads(url = "https://www.yellowpages.com/search?search_terms=coffee&geo_location",extractors = 'divs' , configs = None )
+    results = DOMLeadExtractor.scrape_leads(url = "https://www.yellowpages.com/search?search_terms=coffee&geo_location",extractors = 'address' , configs = None )
+    
+    return {"message": "DOM leads scraped","Results":results}
+
+@router.get("/extract_from_table")
+def DOMLEADS():
+    results = DOMLeadExtractor.scrape_leads(url = "https://www.yellowpages.com/search?search_terms=coffee&geo_location",extractors = 'tables' , configs = None )
     
     return {"message": "DOM leads scraped","Results":results}
 
--- lead_generator/__init__.py
+++ lead_generator/__init__.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 from Routes.DOMLeadsRoute import router
 
 app = FastAPI()
-app.get('/')
+
+@app.get('/')
 def welcome_message():
     return{"message":"welcome to the DOM Leads Extractor Python API Main Route"}
 
 app.include_router(router)
 # This will execute when the script is run directly
+
 if __name__ == "__main__":
     print("API is running")
-# uvicorn lead_generator:app --reload
+# uvicorn __init__:app --reload
