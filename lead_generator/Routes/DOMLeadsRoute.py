272a770e3d0e34dfe3232ba0cd21e9580a1657ca
AWERDdev <kokorami565@gmail.com>
2025-07-31 17:11:46 +0300

    lead generatir  updated

--- lead_generator/Routes/DOMLeadsRoute.py
+++ lead_generator/Routes/DOMLeadsRoute.py
@@ -1,5 +1,24 @@
 from fastapi import APIRouter
 from Modules import DOMLeadExtractor 
+from pydantic import BaseModel
+
+class configs(BaseModel):
+    element_class:str | None = None
+    source_element:str | None = None
+    source_class:str | None = None
+    element_name:str | None = None
+    fields:str | None = None
+    row_element:str | None = None
+    cell_element:str | None = None
+    header_rows:str | None = None
+    patterns:str | None = None
+    item_element:str | None = None
+    image_element:str | None = None
+    caption_element:str | None = None
+    required_attrs:str | None = None
+    attributes:str | None = None
+    extractors:str | None = None
+
 
 router = APIRouter(
     prefix="/DOMLeads",
@@ -12,11 +31,25 @@ def Welcome_Message():
     return{"message":"welcome to the DOM Leads Extractor"}
 
 
-@router.get("/Divs/{url}")
+@router.post("/config")
 async def DOMLEADS( url: str ):
-    results = DOMLeadExtractor.scrape_leads(url , extractors = 'divs' , configs = None )
+    config = {
+        "divs": {
+        "selector": "div.contact-card",
+        "name_selector": ".full-name",
+        "email_selector": ".email-address",
+        "source": "custom-divs"
+    }}
+    results = DOMLeadExtractor.scrape_leads(url , extractors = 'divs' , configs = config )
+    print(f"results from the scraper {results}")
     return {"message": "DOM leads scraped","Results":results}
 
+@router.post("/divs/{url}")
+def DOMLEADS(url:str , config:configs  ):
+    soup = DOMLeadExtractor.get_soup(url)
+    results = DOMLeadExtractor.extract_from_divs(soup, element_class = config.element_class, source_element = config.source_element, source_class = config.source_class)
+   
+    return {"message": "DOM leads scraped","Results":results}
 
 @router.get("/paragraphs")
 def DOMLEADS():
